ed2392f02d09454b0b62cf20f336faae76904575
Satria-WS <extencezone02@gmail.com>
2022-05-01 16:58:49 +0700

    f

---
+++ Class components vs Hooks/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ClassComponent from "./ClassComponent";
+import FunctionalComponent from "./FunctionalComponent";
+
+class App extends React.Component {
+  render() {
+    return (
+    <div>
+      <ClassComponent />
+      <FunctionalComponent />
+    </div>
+      
+      )
+  }
+}
+
+export default App;
